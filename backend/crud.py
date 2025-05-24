754f21a5cea97ea5c2a6a1a2374c93e0b6ef7f29
takuyatokimasa <takuya.tokimasa@anyflow.co.jp>
2025-05-24 15:13:58 +0900

    販売データの取得および作成機能を追加

--- backend/crud.py
+++ backend/crud.py
@@ -33,3 +33,24 @@ def create_user(session: Session, user: schemas.UserCreate):
     session.commit()
     session.refresh(db_user)
     return db_user
+
+
+def get_sales(session: Session):
+    return session.exec(select(models.Sales)).all()
+
+
+def get_sales_by_year(session: Session, year: int):
+    statement = select(models.Sales).where(models.Sales.year == year)
+    return session.exec(statement).all()
+
+
+def create_sales(session: Session, sales: schemas.SalesCreate):
+    db_sales = models.Sales(
+        year=sales.year,
+        department=sales.department,
+        sales=sales.sales,
+    )
+    session.add(db_sales)
+    session.commit()
+    session.refresh(db_sales)
+    return db_sales
