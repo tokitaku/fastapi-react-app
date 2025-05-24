3875ed324829096ed0081a8478d868b519160253
takuyatokimasa <takuya.tokimasa@anyflow.co.jp>
2025-05-24 14:21:44 +0900

    backendをリセット

--- backend/models.py
+++
@@ -1,8 +0,0 @@
-from sqlmodel import SQLModel, Field
-
-
-class Hero(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(index=True)
-    secret_name: str
-    age: int | None = None
