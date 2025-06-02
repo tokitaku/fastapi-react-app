76ff2500faf457cd670b83da8517b2118e74a222
takuyatokimasa <takuya.tokimasa@anyflow.co.jp>
2025-06-02 21:23:08 +0900

    feat: ReadDatabaseおよびSelectYearコンポーネントの追加とデータ取得機能の実装

---
+++ front/src/components/elements/ReadDatabase.js
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { useReadSales } from "../hooks/useReadSales";
+
+// データを変換する関数
+const transformSalesData = (data) => {
+  const departments = ["department", ...data.map(item => item.department)];
+  const years = ["year", ...data.map(item => item.year)];
+  const sales = ["sales", ...data.map(item => item.sales)];
+  
+  return [departments, years, sales];
+};
+
+export const ReadDatabase = ({ year, handleDataChange }) => {
+  const { onClickReadSales } = useReadSales();
+  useEffect(() => {
+    if (year) {
+      const promise = onClickReadSales(year);
+      
+      promise.then((data) => {
+        if (data.length > 0) {
+          const transformedData = transformSalesData(data);
+          handleDataChange(transformedData);
+        } else {
+          console.log("データがありません");
+          handleDataChange([]);
+        }
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [year]);
+
+
+
+  return (
+    <div>
+      <button>データを取得</button>
+    </div>
+  );
+};
---
+++ front/src/components/hooks/useReadSales.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+export const useReadSales = () => {
+  const baseEndpoint = "http://127.0.0.1:8000/sales/";
+  const onClickReadSales = async (year) => {
+    const endpoint = baseEndpoint + year;
+    const res = await axios.get(endpoint);
+    return res.data;
+  };
+
+  return { onClickReadSales };
+};
