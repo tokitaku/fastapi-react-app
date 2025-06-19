7548fc6e551e3488b9d69da60d82d28980c59925
takuyatokimasa <takuya.tokimasa@anyflow.co.jp>
2025-06-19 17:47:16 +0900

    設定ファイルの権限を更新し、Viteの設定を修正しました

--- front/vite.config.js
+++ front/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   esbuild: {
-    loader: 'jsx',
-    include: /src\/.*\.js$/,
+    loader: "jsx",
+    include: /src\/.*\.[jt]sx?$/,
   },
 });
